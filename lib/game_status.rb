5fd67ca9c987eec04942abaade18455ee8b1e7ec
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-16 17:13:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,14 +17,14 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.find do |win_combo|
      board[win_combo[0]] == board[win_combi[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
          return WIN_COMBINATIONS
-
   end
 end
 
 def full?(board)
-  board
-
+  board.none? do |i|
+    i == " " || i.nil?
+  end
 end
