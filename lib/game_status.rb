c828442d22c516c36a3ae77a9418a10a9fe7eaf7
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-16 17:21:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,4 +31,10 @@ end
 
 def draw?(board)
   won?(board) == nil && full?(board) == true
+  return true
+end
+
+def over?(board)
+  won?(board) == true || draw?(board) == true || full?(board) == true
+  return true
 end
