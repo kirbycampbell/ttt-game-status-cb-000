65fb98b87eb3684b5e703d8c59c416b9ab6bc7f3
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-16 17:04:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,12 +17,8 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if WIN_COMBINATIONS.each do |win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
     board[win_combo[0]] == board[win_combi[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
-        return true
-    else 
-        false
-    end
+
   end
 end
-
